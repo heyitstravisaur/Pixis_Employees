afa8a280bcc532ba7eba946e5f79a2203cf8f155
heyitstravisaur <dunk.travis@gmail.com>
2024-11-06 13:11:02 -0600

    created a new test branch because I did funky stuff to recover AJ branch

--- Pixis_Employees/Form1.cs
+++ Pixis_Employees/Form1.cs
@@ -16,5 +16,10 @@ namespace Pixis_Employees
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //hello world
+        }
     }
 }
