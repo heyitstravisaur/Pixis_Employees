e9d11e20038db93cd39ee9735afbaee8aa331582
pittmanJ1992 <joshpittman73@yahoo.com>
2024-11-08 12:44:20 -0600

    Update FlightStaff.cs
    updated this stupid shit on the website because it doesnt wanna upload from visual studio. i really fucking hate github. :)

--- Pixis_Employees/FlightStaff.cs
+++ Pixis_Employees/FlightStaff.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IBM.Data.DB2.iSeries;
 
 namespace Pixis_Employees
 {
+    
     public partial class FlightStaff : Form
     {
+        private BindingSource bindingSource = new BindingSource();
+        private iDB2DataAdapter dataAdapter = new iDB2DataAdapter();
+        DataTable table;
         private PyxisairFlightReservationSystem pfrs;
 
         public FlightStaff(PyxisairFlightReservationSystem form)
@@ -28,7 +33,21 @@ namespace Pixis_Employees
 
         private void FlightStaff_Load(object sender, EventArgs e)
         {
+            string connectionString = "DataSource=deathstar.gtc.edu";
+            string sql = "SELECT * FROM CREW";
 
+            try
+            {
+                dataGridView1.DataSource = bindingSource;
+            
+                dataAdapter = new iDB2DataAdapter(sql, connectionString);
+                iDB2CommandBuilder commandBuilder = new iDB2CommandBuilder(dataAdapter);
+                table = new DataTable();
+            
+                dataAdapter.Fill(table);
+                bindingSource.DataSource = table;
+            }
+            catch (Exception ex) { }
         }
     }
 }
