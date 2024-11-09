d6d929b555c607c5501a9a93e2928073c7fb6371
Kronos27563 <johnsona184@mail.gtc.edu>
2024-11-09 11:07:28 -0600

    Started working on the UpdateEmp class.

--- Pixis_Employees/UpdateEmp.cs
+++ Pixis_Employees/UpdateEmp.cs
@@ -9,9 +9,20 @@ namespace Pixis_Employees
 {
     internal class UpdateEmp
     {
+        public string empNum;
+
         public static void update(List<GroupBox> groupBoxes)
         {
-            
+            foreach (var group in groupBoxes)
+            {
+                foreach (Control control in group.Controls)
+                {
+                    if (control is TextBox textBox)
+                    {
+                        
+                    }
+                }
+            }
         }
     }
 }
