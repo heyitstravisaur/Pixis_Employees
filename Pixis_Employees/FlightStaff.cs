011cf003fa418df4bdc0851cb204b9091c9c49c9
pittmanj1992 <joshpittman73@yahoo.com>
2024-11-20 17:56:02 -0600

    step 3 code is complete, might pretty up the design later

--- Pixis_Employees/FlightStaff.cs
+++ Pixis_Employees/FlightStaff.cs
@@ -13,66 +13,73 @@ namespace Pixis_Employees
 {
     public partial class FlightStaff : Form
     {
+        // connection string details to connect to the db2 database
+        // variable reference to navigate back to the main form when user exits this form
         string connectionString = "Data Source=deathstar.gtc.edu;User ID=itpa641;Initial Catalog=S101FF5C";
         private PyxisairFlightReservationSystem pfrs;
 
+        // constructor
         public FlightStaff(PyxisairFlightReservationSystem form)
         {
             InitializeComponent();
             pfrs = form;
         }
 
+        // exit button to close this form, and to show the main form when closed
         private void btnExit_Click(object sender, EventArgs e)
         {
             pfrs.Show();
             this.Close();
         }
 
+        // load event handler
         private void FlightStaff_Load(object sender, EventArgs e)
         {
-            //LoadFlightNumbers();
+            // calls this method to populate the listbox with distinct flight numbers from 
+            // the STAFFSCHED table
             PopulateListBox(lboxStaff, "SELECT DISTINCT FLIGHTNO FROM STAFFSCHED");
         }
+
+        // listbox selection on click
         private void lboxStaff_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-            //if (lboxStaff.SelectedIndex == null) return;
-
-            //string selectedFlightNo = lboxStaff.SelectedItem.ToString();
-            //LoadEmployeeDetails(selectedFlightNo);
-
+            // when an item is selected in the listbox, calls the method and passes
+            // the selected item to the method
             if (lboxStaff.SelectedItem is string selectedFlightNo)
             {
                 LoadEmployeeDetails(selectedFlightNo);
             }
         }
+
+        // reset button, when clicked repopulates the listbox by calling the 
+        // populatelistbox method again
         private void btnReset_Click(object sender, EventArgs e)
         {
-            //try
-            //{
-            //    lboxStaff.Items.Clear();
-
-            //    LoadFlightNumbers();
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show("Error resetting listbox: " + ex.Message);
-            //}
-
             PopulateListBox(lboxStaff, "SELECT DISTINCT FLIGHTNO FROM STAFFSCHED");
         }
 
+        // populatelistbox method which does an sql query to then populate the provided listbox
         private void PopulateListBox(ListBox listBox, string query, Dictionary<string, object> parameters = null)
         {
             try
             {
+                // clears the listbox
                 listBox.Items.Clear();
 
+                // creates a connection to the database
+                // the reason i went with using instead was i read a bit about how
+                // using will automatically close the connection when the block ended.
+                // but it im not sure if the extra code is all that worth it
                 using (var connection = new iDB2Connection(connectionString))
                 {
+                    // opens db connection
                     connection.Open();
 
+                    // creates a command object to execute the sql query
                     using (var command = new iDB2Command(query, connection))
                     {
+                        // checks that param is not null, then loops thru each key-value pair
+                        // in the parameters dict, then adds the key-value pair to the command object
                         if (parameters != null)
                         {
                             foreach (var param in parameters)
@@ -80,6 +87,12 @@ namespace Pixis_Employees
                                 command.Parameters.AddWithValue(param.Key, param.Value);
                             }
                         }
+
+                        // executes the sql, reads the results row by row, Read() called in a loop to advance the reader
+                        // as long as it returns true, then converts from object to string and adds to listbox
+                        // a lot of this is really confusing and took a lot of time to get right. i think my
+                        // python class helped fill in a lot of blanks but def didnt know a lot of this 
+                        // might not remember a lot of it either
                         using (var reader = command.ExecuteReader())
                         {
                             while (reader.Read())
@@ -92,17 +105,24 @@ namespace Pixis_Employees
             }
             catch (Exception ex)
             {
+                // error handling in case theres issues populating the list
                 MessageBox.Show("Error populating list: " + ex.Message);
             }
         }
 
+        // loademployeedetails method which is to load the employee details
         private void LoadEmployeeDetails(string flightNo)
         {
+            // sql query that joins both STAFFSCHED and EMPLOYEE tables
+            // to reduce on code needed, which took forever to figure out
+            // || ' ' || combines first and last name into a single string as fullname
+            // @ makes the sql search more specific
             string query = @"SELECT E.EFNAME || ' ' || E.ELNAME AS FullName
                             FROM STAFFSCHED S
                             JOIN EMPLOYEE E ON S.EMPNO = E.EMPNO
                             WHERE S.FLIGHTNO = @FlightNo";
 
+            // parameter dictionary for passing in flexible way as dictionaries are immutable
             var parameters = new Dictionary<string, object>
             {
                 { "@FlightNo", flightNo }
@@ -115,98 +135,7 @@ namespace Pixis_Employees
                 lboxStaff.Items.Add("No employess found for this flight");
             }
         }
-            //private void LoadFlightNumbers()
-            //{
-            //    string sqlFlightNumbers = "SELECT DISTINCT FLIGHTNO FROM STAFFSCHED";
-
-            //    using(iDB2Connection connection = new iDB2Connection(connectionString))
-            //    {
-            //        try
-            //        {
-            //            connection.Open();
-            //            using(iDB2Command command = new iDB2Command(sqlFlightNumbers, connection))
-            //            using(iDB2DataReader reader = command.ExecuteReader())
-            //            {
-            //                lboxStaff.Items.Clear();
-
-            //                while(reader.Read())
-            //                {
-            //                    lboxStaff.Items.Add(reader["FLIGHTNO"].ToString());
-            //                }
-            //            }
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            MessageBox.Show("Error loading flight number: " +  ex.Message);
-            //        }
-            //    }
-            //}
-
-
-
-            //private void LoadEmployeeDetails(string flightNo)
-            //{
-            //    string sqlStaffSched = "SELECT EMPNO FROM STAFFSCHED WHERE FLIGHTNO = @FlightNo";
-            //    string sqlEmployee = "SELECT EFNAME, ELNAME FROM EMPLOYEE WHERE EMPNO = @EmpNo";
-
-            //    List<string> empNoList = new List<string>();
-            //    List<string> employeeNames = new List<string>();
-
-            //    using(iDB2Connection connection = new iDB2Connection(connectionString))
-            //    {
-            //        try
-            //        {
-            //            connection.Open();
-
-            //            using(iDB2Command command = new iDB2Command(sqlStaffSched, connection))
-            //            {
-            //                command.Parameters.AddWithValue("@FlightNo", flightNo);
-
-            //                using(iDB2DataReader  reader = command.ExecuteReader())
-            //                {
-            //                    while (reader.Read())
-            //                    {
-            //                        empNoList.Add(reader["EMPNO"].ToString());
-            //                    }
-            //                }
-            //            }
-
-            //            foreach(var empNo in empNoList)
-            //            {
-            //                using(iDB2Command command = new iDB2Command(sqlEmployee, connection))
-            //                {
-            //                    command.Parameters.AddWithValue("@EmpNo", empNo);
-
-            //                    using(iDB2DataReader reader = command.ExecuteReader())
-            //                    {
-            //                        if (reader.Read())
-            //                        {
-            //                            string fullName = $"{reader["EFNAME"]} {reader["ELNAME"]}";
-            //                            employeeNames.Add(fullName);
-            //                        }
-            //                    }
-            //                }
-            //            }
-
-            //            lboxStaff.Items.Clear();
-            //            if(employeeNames.Count > 0)
-            //            {
-            //                foreach(var name in employeeNames)
-            //                {
-            //                    lboxStaff.Items.Add(name);
-            //                }
-            //            }
-            //            else
-            //            {
-            //                lboxStaff.Items.Add("No employees found for the selected flight.");
-            //            }
-            //        }
-            //        catch(Exception ex)
-            //        {
-            //            MessageBox.Show("Error loading employee details: " +  ex.Message);
-            //        }
-            //    }
-            //}
+            
      
             private void btnFlightStaff_Click(object sender, EventArgs e)
             {
