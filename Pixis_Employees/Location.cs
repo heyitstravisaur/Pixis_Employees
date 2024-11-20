2738a9c41e3a040416fdceb70cdd46c70f7b6a3a
izactoknife <izacordonez@gmail.com>
2024-11-20 14:47:25 -0600

    The design for the form is set and the code to fill the listbox with the data from the zipcode table is set

--- Pixis_Employees/Location.cs
+++ Pixis_Employees/Location.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IBM.Data.DB2.iSeries;
+
 
 namespace Pixis_Employees
 {
     public partial class Location : Form
     {
+        iDB2Connection conn;
+        iDB2DataAdapter adapter;
+        DataSet dataset;
         private PyxisairFlightReservationSystem pfrs;
 
         public Location(PyxisairFlightReservationSystem form)
@@ -24,6 +29,37 @@ namespace Pixis_Employees
         {
             pfrs.Show();
             this.Close();
+        }
+
+        private void listBoxCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+            string sql;
+            try
+            {
+                conn = new iDB2Connection("DataSource=10.250.0.30");
+                sql = "SELECT ZIP, CITY, STATE FROM ZIPCODE";
+                adapter = new iDB2DataAdapter(sql, conn);
+
+                dataset = new DataSet();
+                adapter.Fill(dataset);
+
+                listBoxZipCodeTable.Items.Clear();
+                listBoxZipCodeTable.Items.Add("-------------------------------------Zip Code, City, State--------------------------------------------");
+                foreach (DataRow pRow in dataset.Tables[0].Rows)
+                    listBoxZipCodeTable.Items.Add(pRow[0] + "         " + pRow[1] + "        " + pRow[2]);
+                listBoxZipCodeTable.Items.Add("---------------------------------------------------------------------------------------------------------");
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                listBoxZipCodeTable.Items.Add(ex.Message);
+            }
         }
     }
 }
