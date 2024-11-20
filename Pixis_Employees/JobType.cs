40917ed54fe4b19eb0ac76c7f6ac931ecaf2c8f4
izactoknife <izacordonez@gmail.com>
2024-11-20 12:57:11 -0600

    i put a listbox and a display button for the jobtype form

--- Pixis_Employees/JobType.cs
+++ Pixis_Employees/JobType.cs
@@ -25,5 +25,10 @@ namespace Pixis_Employees
             pfrs.Show();
             this.Close();
         }
+
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
