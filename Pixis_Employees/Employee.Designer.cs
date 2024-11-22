f15735bea975bb60f536c5d280923dcd1420960b
heyitstravisaur <dunk.travis@gmail.com>
2024-11-21 18:17:00 -0600

    forgot what i changed

--- Pixis_Employees/Employee.Designer.cs
+++ Pixis_Employees/Employee.Designer.cs
@@ -161,6 +161,7 @@
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(861, 270);
             this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // rEGIONIDDataGridViewTextBoxColumn
             // 
--- Pixis_Employees/Employee.cs
+++ Pixis_Employees/Employee.cs
@@ -357,5 +357,10 @@ namespace Pixis_Employees
                 }
             }
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
